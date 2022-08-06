38a4901de5207784ec634098bae1a77816c4d89a
Mikhail Nikolaev <34393347+mi-doc@users.noreply.github.com>
2022-08-06 12:37:02 +0300

    Fixed wsgi.py

--- average_forecast/average_forecast/wsgi.py
+++ average_forecast/average_forecast/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'average_forecast.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'average_forecast.average_forecast.settings')
 
 application = get_wsgi_application()
