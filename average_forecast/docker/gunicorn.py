4bdf16a9df07540441661efc72638bf73dc5356e
Mikhail Nikolaev <34393347+mi-doc@users.noreply.github.com>
2022-08-06 18:12:34 +0300

    Dockerfile rewritten

---
+++ average_forecast/docker/gunicorn.py
@@ -0,0 +1,12 @@
+import os
+name = 'average_forecast'
+bind = '0.0.0.0:8000'
+proc_name = 'average_forecast'
+daemon = False
+user = 'root'
+group = 'root'
+
+workers = 1
+
+errorlog = '-'
+accesslog = '-'
